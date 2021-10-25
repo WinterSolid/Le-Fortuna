7bf0580630ca5765dedaf193b0948b711820254b
WinterSolid <58896705+WinterSolid@users.noreply.github.com>
2021-10-25 11:11:35 -0500

    Commit project - Le Fortuna
    Fortune teller app

---
+++ Le Fortuna/Le Fortuna/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Le Fortuna
+//
+//  Created by WinterSolid
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var fortuneTextView: UITextView!
+    
+    let Phrases = [
+   "Hell Yes",
+     "It has been decided",
+     "Without a doubt",
+    " Yes definitely ",
+    " You may rely on it ",
+    " As I see it, yes ",
+    " Most likely ",
+    " Outlook good ",
+    " Yes ",
+    " Signs point to yes",
+    " Reply hazy, try again",
+    " Ask again later",
+    " Better not tell you now",
+    " Better not to know",
+    " Concentrate harder",
+    " Don't count on it",
+    " My reply is no",
+    " My forces say no",
+    " Outlook is bleak",
+    " Very doubtful",
+     ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func pressMeButtonPressed(_ sender: UIButton) {
+        fortuneTextView.text = Phrases.randomElement()
+        print("Button Tapped")
+    }
+    
+}
+
